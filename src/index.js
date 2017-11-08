754d425e0cfd231ea017167fe1b4fba448d22aed
Pedro S. Moreira <pedsmoreira@gmail.com>
2017-11-08 00:35:16 -0200

    Fix component props import

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import type { Props as FormProps } from "./Form";
 import Field from "./Field";
 import type { Props as FieldProps } from "./Field";
 
-import type { ComponentProps } from "./ComponentProps";
+import type { ComponentProps } from "./ComponentProps.flow";
 
 import FieldGroup from "./FieldGroup";
 import fieldDecorator from "./fieldDecorator";
