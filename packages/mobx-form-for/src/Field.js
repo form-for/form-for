ef2d46851f46c4176567c8717e2f20f57c13f7fd
Pedro S. Moreira <pedsmoreira@gmail.com>
2018-03-03 05:05:04 -0300

    Remove extension on mobx Field class

--- packages/mobx-form-for/src/Field.js
+++ packages/mobx-form-for/src/Field.js
@@ -1,11 +1,6 @@
 // @flow
 
-import * as React from 'react';
-import { observe } from 'mobx';
 import { observer } from 'mobx-react';
-import PropTypes from 'prop-types';
-import { Field as BaseField } from 'form-for';
-
-class Field extends BaseField {}
+import { Field } from 'form-for';
 
 export default observer(Field);
