3575773270ec8e84997f6c85ad473c127ad9e723
Pedro S. Moreira <pedsmoreira@gmail.com>
2018-03-27 18:52:30 -0300

    Fix fieldDecorator

--- packages/form-for/src/fieldDecorator.js
+++ packages/form-for/src/fieldDecorator.js
@@ -3,13 +3,16 @@
 function decorate(target: any, key: string, descriptor: any, properties: { [_: string]: any }) {
   if (!target.schema) target.schema = {};
   target.schema[key] = properties;
-  descriptor.writable = true;
+
+  return {
+    writable: true
+  };
 }
 
 export default function fieldDecorator(targetOrProperties: any, key?: string, descriptor?: any) {
-  if (key) {
-    return decorate(targetOrProperties, key, descriptor, {});
-  }
+  // @field - without params
+  if (key) return decorate(targetOrProperties, key, descriptor, {});
 
+  // @field({ }) - with params
   return (target: any, key: string, descriptor: any) => decorate(target, key, descriptor, targetOrProperties);
 }
